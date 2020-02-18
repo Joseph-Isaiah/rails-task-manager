05385656c64c90bb42709974341406d55304df53
Joseph Isaiah <jos.mcnamara@outlook.com>
2020-02-18 16:17:15 -0600

    update tp the restaurant task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,13 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
   def edit
     @task = Task.find(params[:id])
   end
@@ -26,7 +33,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    # redirect_to tasks_path
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: 'new'
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
-  post 'tasks', to: 'tasks#create'
+  post 'tasks', to: 'tasks#create', as: 'create'
 
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   patch 'tasks/:id', to: 'tasks#update'
